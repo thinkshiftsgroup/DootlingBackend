3d64fe97fb96822faaa7a2ba72db8e3aa56bab27
denzubechi <chukwumasamuel371@gmail.com>
2025-12-04 13:08:42 +0100

    prisma

--- src/prisma/migrations_temp/20251127171907_update_category_model/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "ProductPricing" ADD COLUMN     "discountEndDate" TIMESTAMP(3),
-ADD COLUMN     "discountStartDate" TIMESTAMP(3),
-ADD COLUMN     "discountedSellingPrice" DOUBLE PRECISION;
--- src/prisma/migrations_temp/add_subscribe_to_marketing/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "subscribeToMarketing" BOOLEAN NOT NULL DEFAULT false;
